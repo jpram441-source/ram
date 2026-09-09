7ff03ecca61f622e0e1dc69ad9a9e91485dee220
jpram441-source <jpram441@gmail.com>
2026-09-09 18:41:25 +0530

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host="localhost"; 
-$user="root"; 
-$pass=""; 
-$db="school_erp";
-$conn = new mysqli($host, $user, $pass, $db);
-if($conn->connect_error){ die("DB Connection Failed"); }
-session_start();
-?>
